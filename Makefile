c42ccb4b27c43f3cec2e9826f436a69513663358
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-04-24 19:27:53 +0100

    update

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 run-dynamodb:
 	docker kill dynamodb-lookup || true
 	docker run --rm -d --name dynamodb-lookup -p 8000:8000 amazon/dynamodb-local
+
+build:
+	env GOOS=linux go build -ldflags="-s -w" -o bin/connecntionmanager ./connecntionmanager
+
+deploy-local:
+	AWS_PROFILE=personal sls deploy -s dev
--- websocket.js
+++ websocket.js
@@ -1,23 +1,14 @@
-// const io = require("socket.io-client");
-
-// const socket = io("wss://tok1tuxpge.execute-api.eu-west-2.amazonaws.com/dev");
-// console.log('after')
-// socket.on("connect_error", (data) => {
-//   console.log('connect error', data);
-// });
-// socket.on("connect", (data) => {
-//   console.log('connect', data);
-// });
 const WebSocket = require("ws");
 
-// try {
-  const ws = new WebSocket(
-    "wss://tok1tuxpge.execute-api.eu-west-2.amazonaws.com/dev"
+var ws;
+try {
+  ws = new WebSocket(
+    "APIGWWebsocketEndpoint"
   );
-// } catch (e) {
-//     console.log('helloe')
-//   console.log(e);
-// }
+} catch (e) {
+    console.log('helloe')
+  console.log(e);
+}
 
 ws.on("open", () => console.log("connected"));
 ws.on("message", (data) => console.log(`From server: ${data}`));
@@ -25,11 +16,3 @@ ws.on("close", () => {
   console.log("disconnected");
   process.exit();
 });
-
-// ws.on("open", function open() {
-//   ws.send("something");
-// });
-
-// ws.on("message", function incoming(data) {
-//   console.log(data);
-// });
