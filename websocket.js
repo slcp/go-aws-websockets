e9f126bffddac3fcc7857b251a271b996d56696f
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-04-25 19:18:48 +0100

    use env for websocket url

--- websocket.js
+++ websocket.js
@@ -2,15 +2,23 @@ const WebSocket = require("ws");
 
 var ws;
 try {
-  ws = new WebSocket(
-    "APIGWWebsocketEndpoint"
-  );
+  ws = new WebSocket(process.env.WEBSOCKET_ENDPOINT);
 } catch (e) {
-    console.log('helloe')
+  console.log("helloe");
   console.log(e);
 }
 
-ws.on("open", () => console.log("connected"));
+ws.on("open", () => {
+  ws.send(
+    JSON.stringify({
+      action: "customaction",
+      data: {
+        hello: "world",
+      },
+    })
+  );
+  console.log("connected");
+});
 ws.on("message", (data) => console.log(`From server: ${data}`));
 ws.on("close", () => {
   console.log("disconnected");
